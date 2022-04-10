a0dcf2ce0873ced9fdc8559d7364ceca73d4e515
kk <kei.kand@live.jp>
2022-04-10 16:00:26 +0200

    loaded

--- src/app/get-page/get-page.component.ts
+++ src/app/get-page/get-page.component.ts
@@ -17,8 +17,8 @@ export class GetPageComponent implements OnInit {
   curl_service_endpoint="https://apius.reqbin.com/";
   target_url="news.yahoo.co.jp";
 
-  //function_url="http://localhost:5021/knowledge-base1/asia-northeast1/curltool"
-  function_url="https://asia-northeast1-knowledge-base1.cloudfunctions.net/curltool"
+  function_url="http://localhost:5021/knowledge-base1/asia-northeast1/curltool"
+  //function_url="https://asia-northeast1-knowledge-base1.cloudfunctions.net/curltool"
 
   constructor(private fns: Functions,private sanitizer: DomSanitizer,private http:HttpClient) { 
     this.get_curl= httpsCallableData(fns, 'curltool', { timeout: 3_000 })
@@ -29,6 +29,7 @@ export class GetPageComponent implements OnInit {
     .subscribe()*/
 
     this.reload()
+
   }
 
   ngOnInit(): void {
@@ -45,8 +46,33 @@ export class GetPageComponent implements OnInit {
       //catchError(e=>console.log(e))
       tap(content=>{
         //if(white_domains.includes())
-        this.src_doc=this.sanitizer.bypassSecurityTrustHtml(content)
+        this.src_doc=this.sanitizer.bypassSecurityTrustHtml(content);
+        var interval = setTimeout(() => {
+          this.siteLoaded();
+        }, 3000);
       })
     ).subscribe();
+  }
+  siteLoaded(){
+ // $0.getElementsByTagName("body")[0].addEventListener('click',function(e){console.log(e)})
+ var iframe1=document.getElementsByTagName('iframe');
+ var content1=iframe1[0].contentDocument;
+ if(!content1) return false;
+ var body1=content1.getElementsByTagName('body');
+ body1[0].addEventListener('click',function(e){console.log(e)});
+ console.log(body1[0])
+ var anchors = body1[0].getElementsByTagName("a");
+ for (var i = 0; i < anchors.length ; i++) {
+   anchors[i].addEventListener("click", 
+       function (event) {
+           event.preventDefault();
+           console.log(this.href)
+          // if (confirm('Are you sure?')) {
+              // window.location = this.href;
+          // }
+       }, 
+       false);
+  }
+  return true
   }
 }
